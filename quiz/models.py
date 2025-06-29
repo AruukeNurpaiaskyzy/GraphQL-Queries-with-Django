a356d5dff94b92661985d2474e773238efa3e636
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-30 00:30:00 +0600

    continue models

--- quiz/models.py
+++ quiz/models.py
@@ -2,4 +2,18 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-class Category 
+class Category (models.Model):
+    name  = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+class Quizess (models.Model):
+    title = models.CharField(max_length=255, default=_("New quiz"))
+    category = models.ForeignKey(Category, default = 1, on_delete = models.DO_NOTHING)
+    date_created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+
+class Answer(models.Model):
